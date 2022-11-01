c5ee9b5662157bd2bfcd9ef1d1d51dab425b3db1
DuyThinhh <116861540+DuyThinhh@users.noreply.github.com>
2022-11-01 12:52:47 +0700

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -3,10 +3,10 @@ import yaml
 import sys
 from glob import glob
 
-datavars_fname = sys.argv[1:1] or glob('*.yml')[0]
+thinh_fname = sys.argv[1:1] or glob('*.yml')[0]
 template_fname = sys.argv[2:2] or glob('*.j2')[0]
 
-datavars = yaml.load(open(datavars_fname).read())
+thinh = yaml.load(open(thinh_fname).read())
 template = Template(open(template_fname).read())
 
 print ("template.render(datavars)")
